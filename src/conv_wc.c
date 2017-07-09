01a0402a655a687d6d825850d91c6705e5a280fb
Aaron GUNDRY <agundry@e1z2r7p44.42.us.org>
2017-07-08 17:25:49 -0700

    add few conv fxns

---
+++ src/conv_wc.c
@@ -0,0 +1,21 @@
+#include "ft_print.h"
+
+int	conv_wc(t_buf *buf, t_format *fmt, va_list ap)
+{
+	char	arg[MB_LEN_MAX + 1]; ////////////////////////////
+	size_t	arglen;
+	int		wclen;
+
+	if ((wclen = ft_wctomb(arg, va_arg(ap, wint_t))) == -1) ////////////wctomb
+		return (-1);
+	arg[wclen] = '\0';
+	if ((arglen = ft_strlen(arg)) == 0)
+		++arglen;
+	fmt->prec = -1;
+	if (fmt->min_width > 1 && fmt->f_minus == 0)
+		str_formatting(buf, fmt, arglen);
+	ft_buffer_add(buf, arg, arglen);
+	if (fmt->min_width > 1 && fmt->f_minus == 1)
+		str_formatting(buf, fmt, arglen);
+	return (1);
+}
