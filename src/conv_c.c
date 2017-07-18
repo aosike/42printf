0dc92af361503035dc31cd1b9e9438188a41464a
Aaron GUNDRY <agundry@e1z3r3p18.42.us.org>
2017-07-18 15:58:59 -0700

    conv_c change test

--- src/conv_c.c
+++ src/conv_c.c
@@ -8,7 +8,7 @@ int	conv_c(char buf[BUFF_SIZE], t_format *fmt, va_list vl)
 	if (fmt->no_conv != 0)
 		buf_add(buf, &fmt->no_conv, 1);
 	else
-		buf_add(buf, &(char){(unisigned char)va_arg(vl, int)}, 1); //???????????
+		buf_add(buf, &(char)va_arg(vl, int), 1);
 	if (fmt->min_width > 1 && fmt->f_minus == 1)
 		str_formatting(buf, fmt, 1);
 	return (1);
